45ea550b3f577b0e45c653afd7093e796229bc1c
Dewnaka <lithilakavi@gmail.com>
2024-05-16 19:04:00 +0530

    Created NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Navbar from '../Components/Navbar'; // Importing the Navbar component
+import Navbar from './Components/Navbar'; // Corrected import path
 
 function App() {
   return (
     <div className="App">
-      <Navbar /> {/* Including the Navbar component */}
+      <Navbar />
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import './NavBar.css';
+import './Navbar.css';
 
-import logo from './Media/cart.png';
+import cart from '../Media/cart.png';
+import gymshark from '../Media/Gymshark-Logo-700x394.png'
 
 export default function Navbar() {
   return (
     <div className='navbar'>
-      <div className="nav-logo"> {/* Corrected syntax */}
-        <img src={logo} alt="cart" />
-        <p>SHOPPER</p>
+      <div className="nav-logo">
+        <img src={gymshark} alt="gymshark" />
+        <p>GymShark</p>
+      </div>
+      <ul className="nav-menu">
+        <li>WOMEN</li>
+        <li>MEN</li>
+        <li>ACCESSORIES</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart} alt='cart'/>
       </div>
     </div>
+
   );
 }
