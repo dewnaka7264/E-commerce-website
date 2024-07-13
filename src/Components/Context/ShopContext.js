c18c6069ea597d40a8f661504f06b2b52e6f7fd5
Dewnaka <lithilakavi@gmail.com>
2024-07-13 19:26:50 +0530

    Created ShopContext.js

--- src/Components/Context/ShopContext.js
+++ src/Components/Context/ShopContext.js
@@ -1,7 +1,14 @@
-import React, {createContext} from "react";
-
+import React, {createContext,useContext} from "react";
+import allproducts from "../allproducts";
 export const ShopContext = createContext(null);
 
 const ShopContextProvider =(props)=>{
-    const contextValue = {}
+    const contextValue = {allproducts};
+
+    return(
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
 }
+export default ShopContextProvider;
