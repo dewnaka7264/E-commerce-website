499ec7110a7233aaf7d4a070298cb85089f3b588
Dewnaka <lithilakavi@gmail.com>
2024-08-10 17:40:24 +0530

    Warning alert set to close when clicked close button

--- src/Components/productDisplay/WarningAlert.js
+++ src/Components/productDisplay/WarningAlert.js
@@ -2,13 +2,17 @@ import * as React from 'react';
 import Alert from '@mui/material/Alert';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import {useState} from "react";
 
 export default function WarningAlert() {
+    const [open, setOpen] = useState(true);
     return (
         <Stack sx={{ width: '100%' }} spacing={2}>
-            <Alert severity="warning" onClose={() => {}}>
-                Please select size and quantity.
-            </Alert>
+            {open && (
+                <Alert severity="warning" onClose={() => setOpen(false)}>
+                    Please select size and quantity.
+                </Alert>
+            )}
 
         </Stack>
     );
